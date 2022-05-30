9d5f82d46f6cd7f63b86f16370d8c7dcfaeba486
Adrian Calero <acalerobarrero@gmail.com>
2022-05-30 18:49:27 +0200

    Adding render template of card

---
+++ my-react/src/App.js
@@ -0,0 +1,128 @@
+import "./App.css";
+import { Card } from "./component/card";
+
+function App() {
+    return (
+        <div className="container">
+            <header className="main-header">
+                <h1 className="main-title">The pointing gentlemen</h1>
+            </header>
+            <section className="controls">
+                <p className="info">0 gentlemen pointing at you</p>
+                <button className="button button--select">Select all</button>
+            </section>
+            <main className="main">
+                <ul className="gentlemen">
+                    <Card></Card>
+                    {/* <li class="gentleman">
+                        <div class="gentleman__avatar-container">
+                            <img
+                                class="gentleman__avatar"
+                                src="/img/fary.jpg"
+                                alt="The Fary pointing at you"
+                            />
+                            <span class="gentleman__initial">F</span>
+                        </div>
+                        <div class="gentleman__data-container">
+                            <h2 class="gentleman__name">The Fary</h2>
+                            <ul class="gentleman__data-list">
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Profession:
+                                    </span>
+                                    Influencer
+                                </li>
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Status:
+                                    </span>
+                                    RIP
+                                </li>
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Twitter:
+                                    </span>
+                                    Pending
+                                </li>
+                            </ul>
+                        </div>
+                        <i class="icon gentleman__icon fas fa-check"></i>
+                        <i class="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+                    </li>
+                    <li class="gentleman">
+                        <div class="gentleman__avatar-container">
+                            <img
+                                class="gentleman__avatar"
+                                src="/img/julio.jpg"
+                                alt="Julius Churchs pointing at you"
+                            />
+                            <span class="gentleman__initial">J</span>
+                        </div>
+                        <div class="gentleman__data-container">
+                            <h2 class="gentleman__name">Julius Churchs</h2>
+                            <ul class="gentleman__data-list">
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Profession:
+                                    </span>
+                                    Youtuber
+                                </li>
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Status:
+                                    </span>
+                                    Alive
+                                </li>
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Twitter:
+                                    </span>
+                                    @julius_churchs
+                                </li>
+                            </ul>
+                        </div>
+                        <i class="icon gentleman__icon fas fa-check"></i>
+                        <i class="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+                    </li>
+                    <li class="gentleman">
+                        <div class="gentleman__avatar-container">
+                            <img
+                                class="gentleman__avatar"
+                                src="/img/bertin.jpg"
+                                alt="Bertin Osbourne pointing at you"
+                            />
+                            <span class="gentleman__initial">B</span>
+                        </div>
+                        <div class="gentleman__data-container">
+                            <h2 class="gentleman__name">Bertin Osbourne</h2>
+                            <ul class="gentleman__data-list">
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Profession:
+                                    </span>
+                                    Java developer
+                                </li>
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Status:
+                                    </span>
+                                    Alive
+                                </li>
+                                <li class="gentleman__data">
+                                    <span class="gentleman__data-label">
+                                        Twitter:
+                                    </span>
+                                    @osbourne
+                                </li>
+                            </ul>
+                        </div>
+                        <i class="icon gentleman__icon fas fa-check"></i>
+                        <i class="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+                    </li> */}
+                </ul>
+            </main>
+        </div>
+    );
+}
+
+export default App;
---
+++ my-react/src/component/card.js
@@ -0,0 +1,42 @@
+import { chapters } from "../model/dates";
+import "./card.css";
+import { React } from 'react';
+
+export function Card() {
+    return( {chapters.map((item) =>{
+        return(<>
+        <li className="gentleman">
+            <div className="gentleman__avatar-container">
+                <img
+                    className="gentleman__avatar"
+                    src={"/img/" + item.picture}
+                    alt={item.alternativeText}
+                />
+                <span className="gentleman__initial">F</span>
+            </div>
+            <div className="gentleman__data-container">
+                <h2 className="gentleman__name">{item.name}</h2>
+                <ul className="gentleman__data-list">
+                    <li className="gentleman__data">
+                        <span className="gentleman__data-label">
+                            Profession:
+                        </span>
+                        {item.profession}
+                    </li>
+                    <li className="gentleman__data">
+                        <span className="gentleman__data-label">Status:</span>
+                        {item.status}
+                    </li>
+                    <li className="gentleman__data">
+                        <span className="gentleman__data-label">Twitter:</span>
+                        {item.twitter}
+                    </li>
+                </ul>
+            </div>
+            <i className="icon gentleman__icon fas fa-check"></i>
+            <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+        </li>
+        </>
+    )})
+    })    
+}
